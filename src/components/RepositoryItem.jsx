5089a1efc5398f743390beccc879e1001195c256
Davi Cajucaru <davicajucaru@hotmail.com>
2021-09-28 14:09:21 -0300

    add typescript

--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem(props) {
-  return (
-    <li key={props.repository.id}>
-      <strong>{props.repository?.name}</strong>
-      <p>{props.repository?.description}</p>
-      <a href={props.repository?.html_url}>Acessar repositório</a>
-    </li>
-  );
-}
